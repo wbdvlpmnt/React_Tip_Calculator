d57f749d81dace976303259a2a923ec08052fa77
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-12-03 14:54:36 -0600

    Tip calculation with reusable component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,58 @@ import "./App.css";
 
 function App() {
   const [billAmount, setBillAmount] = useState(0);
+  const [satisfaction, setSatisfaction] = useState("");
+  const [friendSatisfaction, setFriendSatisfaction] =
+    useState("not satisfied, 10%");
+  const [yourSatisfaction, setYourSatisfaction] =
+    useState("not satisfied, 10%");
+
+  const lookup = {
+    "not satisfied, 10%": 0.1,
+    "satisfied, 15%": 0.15,
+    "highly satisfied, 20%": 0.2,
+  };
+
+  let yourTip = lookup[yourSatisfaction];
+  let friendsTip = lookup[friendSatisfaction];
+  let totalTip = (yourTip + friendsTip) * billAmount;
+  let totalAmount = Number(billAmount) + Number(totalTip);
 
   function onBillAmountChange(billAmount) {
     setBillAmount(billAmount);
   }
+
+  function onChangeSatisfaction(satisfactionValue, prompt) {
+    if (prompt === "How did you like the service?") {
+      setYourSatisfaction(satisfactionValue);
+    }
+    if (prompt === "How did your friend like the service?") {
+      setFriendSatisfaction(satisfactionValue);
+    }
+  }
+
   return (
     <div>
       <BillAmount
         billAmount={billAmount}
         onBillAmountChange={onBillAmountChange}
       />
-      <ReusableTip>How did you like the service? </ReusableTip>
-      <ReusableTip>How did your friend like the service? </ReusableTip>
-      <h1>You pay $92 ($80+$12 tip)</h1>
+      <ReusableTip
+        yourSatisfaction={yourSatisfaction}
+        onChangeSatisfaction={onChangeSatisfaction}
+      >
+        How did you like the service?
+      </ReusableTip>
+      <ReusableTip
+        friendSatisfaction={friendSatisfaction}
+        onChangeSatisfaction={onChangeSatisfaction}
+      >
+        How did your friend like the service?
+      </ReusableTip>
+      <h1>
+        You pay ${totalAmount} (${billAmount}+$
+        {totalTip} tip)
+      </h1>
       <button>Reset</button>
     </div>
   );
@@ -34,11 +73,27 @@ function BillAmount({ billAmount, onBillAmountChange }) {
   );
 }
 
-function ReusableTip({ children }) {
+function ReusableTip({ satisfaction, onChangeSatisfaction, children }) {
+  const tipOptions = [
+    "not satisfied, 10%",
+    "satisfied, 15%",
+    "highly satisfied, 20%",
+  ];
   return (
     <div>
       <span>{children}</span>
-      <input />
+      <select
+        value={satisfaction}
+        onChange={(e) => onChangeSatisfaction(e.target.value, children)}
+      >
+        {tipOptions.map((opt, index) => {
+          return (
+            <option value={opt} key={index}>
+              {opt}
+            </option>
+          );
+        })}
+      </select>
     </div>
   );
 }
